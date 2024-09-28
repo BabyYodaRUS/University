da036a4bb7a9b9fced4030bbb18c5c550e053754
Михаил Порысев <m.porysev@vk.com>
2024-09-28 03:07:49 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,5 @@
+number_of_hw = 12
+number_of_hours = 1.5
+name = 'Python'
+time_task = (number_of_hours/number_of_hw)
+print ('Курс:',name,', всего задач:',number_of_hw,', затрачено часов:',number_of_hours,', среднее время выполнения:',time_task,'часа.')
